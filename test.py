0e9ed0f895630779f48e6ed1aafd1301b1d1b237
sergey.kostichev <sergey.kostichev@gcore.com>
2025-06-13 16:59:17 +0500

    remove test files

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-r = requests.get('http://localhost:8000/api/criteria/files')
-data = r.json()
-print('Status:', r.status_code)
-print('Files:', len(data['files']))
-print('Products:', data['products']) 
